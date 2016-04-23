16df3090d6b9db9869ee45242d3ea788175bd97e
Shai Wilson <sjwilson2@usfca.edu>
2016-04-23 14:58:47 -0700

    working on fb codelab ex

---
+++ day1.py
@@ -0,0 +1,9 @@
+from pythonds.basic.stack import stack
+
+def parChecker(symbolString):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol == '(':
---
+++ linked_list.py
@@ -0,0 +1,25 @@
+// print linked list backwards
+
+class Node(self):
+    def __init__(self, data=None, next=None):
+        self.data
+        self.next
+
+
+def print_backwards(mylist):
+    if mylist == None: return
+    head = mylist
+    tail = mylist.next
+    print_backwards(tail)
+    print head,
+
+def print_list(mylist):
+    if mylist == None: return
+    while node:
+        print node,
+        node = node.next
+
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
