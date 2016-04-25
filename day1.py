cec11e5adf7f6c303fae2a72ab80f3d8ea280e10
Shai Wilson <sjwilson2@usfca.edu>
2016-04-25 12:57:57 -0700

    add daily ex

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
-from pythonds.basic.stack import stack
+from pythondsimport stack
 
 def parChecker(symbolString):
     s = Stack()
@@ -7,3 +7,20 @@ def parChecker(symbolString):
     while index < len(symbolString) and balanced:
         symbol = symbolString[index]
         if symbol == '(':
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                balanced = False
+            else:
+                s.pop()
+
+        index = index + 1
+
+    if balanced and s.isEmpty:
+        return True
+    else:
+        return False
+
+print(parChecker('((()))'))
+print(parChecker('(()'))
+
